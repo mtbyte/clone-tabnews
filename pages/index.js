10db97dda484875f3d053f0b799e4d00a1145bc7
mt <101599970+mtbyte@users.noreply.github.com>
2023-07-04 02:40:34 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>gigi, eu te amo demais sabia meu amor da minha vida</h1> 
+  return <h1>gigi, eu te amo demais sabia meu amor da minha vida</h1>;
 }
 
 export default Home;
